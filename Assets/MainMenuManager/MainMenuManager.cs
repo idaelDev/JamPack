62757ac397efb3858186c36758be1ba79e909aa1
idaelDev <judicael.abecassis@gmail.com>
2015-10-14 15:07:25 +0200

    Feat(Manager) : add scene and menu managers

---
+++ Assets/MainMenuManager/MainMenuManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuManager : Singleton<MainMenuManager> {
+
+    public CanvasGroup MainCanvasGroup;
+    public CanvasGroup CreditsCanvasGroup;
+    public CanvasGroup TutorialCanvasGroup;
+
+    public int mainSceneId;
+
+    public void OnStartGame()
+    {
+        SceneManager.Instance.SetScene(mainSceneId);
+    }
+
+    public void OnQuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OnCredits()
+    {
+        HideAllCanvas();
+        ShowCanvasGroup(CreditsCanvasGroup, true);
+    }
+
+    public void OnTutorial()
+    {
+        HideAllCanvas();
+        ShowCanvasGroup(TutorialCanvasGroup, true);
+    }
+
+    public void OnMain()
+    {
+        HideAllCanvas();
+        ShowCanvasGroup(MainCanvasGroup, true);
+    }
+
+    void HideAllCanvas()
+    {
+        ShowCanvasGroup(MainCanvasGroup, false);
+        ShowCanvasGroup(CreditsCanvasGroup, false);
+        ShowCanvasGroup(TutorialCanvasGroup, false);
+    }
+
+    void ShowCanvasGroup(CanvasGroup c, bool show)
+    {
+        c.alpha = (show)? 1 : 0;
+        c.interactable = show;
+        c.blocksRaycasts = show;
+    }
+}
---
+++ Assets/SceneManager/SceneManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SceneManager : Singleton<SceneManager> {
+
+    public Color fadeColor = Color.black;
+    public float StartFadeTime = 1.5f;
+    public float EndFadeTime = 1.5f;
+
+    private bool isSceneStarting = true;
+    private GameObject CanvasObject;
+    private CanvasGroup canvasGroup;
+
+    void Awake()
+    {
+        CanvasObject = new GameObject("SceneFader");
+        CanvasObject.transform.SetParent(transform);
+        CanvasObject.AddComponent(typeof(Canvas));
+        CanvasObject.GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
+        CanvasObject.AddComponent(typeof(Image));
+        CanvasObject.AddComponent(typeof(CanvasGroup));
+        canvasGroup = CanvasObject.GetComponent<CanvasGroup>();
+        Image image = CanvasObject.GetComponent<Image>();
+        image.color = fadeColor;
+    }
+
+    void Update()
+    {
+        if(isSceneStarting)
+        {
+            StartCoroutine(StartScene());
+        }
+    }
+
+    public void SetScene(int sceneToLoad)
+    {
+        StartCoroutine(EndScene(sceneToLoad));
+    }
+
+    private IEnumerator EndScene(int sceneToLoad)
+    {
+        float t = 0;
+        while (t < EndFadeTime)
+        {
+            canvasGroup.alpha = Mathf.Lerp(0, 1, t / EndFadeTime);
+            t += Time.deltaTime;
+            yield return 0;
+        }
+        Application.LoadLevel(sceneToLoad);
+    }
+
+    private IEnumerator StartScene()
+    {
+        float t = 0;
+        isSceneStarting = false;
+        while (t < StartFadeTime)
+        {
+            canvasGroup.alpha = Mathf.Lerp(1, 0, t / StartFadeTime);
+            t += Time.deltaTime;
+            yield return 0;
+        }
+    }
+
+}
